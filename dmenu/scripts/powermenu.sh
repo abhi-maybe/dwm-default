5bca430c43159d5526ab5189c57559a05cc1ef09
Abhi <104899412+abhi-maybe@users.noreply.github.com>
2022-10-21 20:31:42 +0530

    powermenu added

---
+++ dmenu/scripts/powermenu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function powermenu {
+	options="Cancel\nShutdown\nRestart\nExit\nLogout"
+	selected=$(echo -e $options | dmenu -p 'powermenu ')
+		if [[ $selected = "Shutdown" ]]; then
+			poweroff
+		elif [[ $selected = "Restart" ]]; then
+			reboot
+		elif [[ $selected = "Logout" ]]; then
+			loginctl terminate-session ${XDG_SESSION_ID-}
+		elif [[ $selected = "Exit" ]]; then
+			killall Xorg
+		elif [[ $selected = "Cancel" ]]; then
+	return
+fi
+}
+
+powermenu
