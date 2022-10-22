be879213a64841540d1b91af5d09f643157cd71e
Abhi <104899412+abhi-maybe@users.noreply.github.com>
2022-10-22 20:00:34 +0530

    2.0.1

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 sudo pacman -Syyu --noconfirm
-sudo pacman -S xorg sl light pamixer brightnessctl git cmake --noconfirm
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S xorg xterm sl light pamixer brightnessctl git cmake --noconfirm
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -S networkmanager-dmenu-git --noconfirm
 sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts --noconfirm
 git clone https://github.com/firstb0ss/dwm-default && cd dwm-default
+cp .xinitrc ~/
 sudo sh install.sh
 sl && reboot
