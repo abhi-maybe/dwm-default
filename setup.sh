0f6efe32cdc6f47139b9fb2f599223ac41559e95
Abhi <104899412+abhi-maybe@users.noreply.github.com>
2022-10-22 20:39:21 +0530

    2.0.4

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syyu --noconfirm
-sudo pacman -S xfce4-power-manager alacritty xorg xterm sl light pamixer brightnessctl git cmake --noconfirm
+sudo pacman -S xfce4-power-manager alacritty xorg xorg-xinit xterm sl light pamixer brightnessctl git cmake --noconfirm
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -S networkmanager-dmenu-git --noconfirm
 sudo pacman -S feh noto-fonts-cjk noto-fonts-emoji noto-fonts --noconfirm
