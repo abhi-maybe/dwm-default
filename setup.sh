3789e40bcfb1883f01a274efc5ad7d7e3dcd5687
Abhi <104899412+abhi-maybe@users.noreply.github.com>
2022-10-22 20:16:45 +0530

    uodate

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 sudo pacman -Syyu --noconfirm
-sudo pacman -S xorg xterm sl light pamixer brightnessctl git cmake --noconfirm
+sudo pacman -S xfce4-power-manager alacritty xorg xterm sl light pamixer brightnessctl git cmake --noconfirm
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -S networkmanager-dmenu-git --noconfirm
-sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts --noconfirm
+sudo pacman -S feh noto-fonts-cjk noto-fonts-emoji noto-fonts --noconfirm
 git clone https://github.com/firstb0ss/dwm-default && cd dwm-default
 cp .xinitrc ~/
 sudo sh install.sh
-sl && reboot
+feh --set-bg wallpaper.jpg
+cp -R .config/ ~/.config
+sl
+startx
