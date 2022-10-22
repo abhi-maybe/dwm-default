fc0379cd3b8efe0744896186be51560a8f556d14
Abhi <104899412+abhi-maybe@users.noreply.github.com>
2022-10-22 19:32:04 +0530

    added script

--- dmenu/scripts/powermenu.sh
+++ dmenu/scripts/powermenu.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 function powermenu {
-	options="Cancel\nShutdown\nRestart\nExit\nLogout"
+	options="Poweroff\nReboot\nLogout\nSuspend\nTUI\nCancel"
 	selected=$(echo -e $options | dmenu -p 'powermenu ')
-		if [[ $selected = "Shutdown" ]]; then
+		if [[ $selected = "Poweroff" ]]; then
 			poweroff
-		elif [[ $selected = "Restart" ]]; then
+		elif [[ $selected = "Reboot" ]]; then
 			reboot
 		elif [[ $selected = "Logout" ]]; then
 			loginctl terminate-session ${XDG_SESSION_ID-}
-		elif [[ $selected = "Exit" ]]; then
+		elif [[ $selected = "Suspend" ]]; then
+			systemctl suspend
+		elif [[ $selected = "TUI" ]]; then
 			killall Xorg
 		elif [[ $selected = "Cancel" ]]; then
 	return
---
+++ install.sh
@@ -0,0 +1,13 @@
+cd dwm
+make
+make install
+cd ..
+cd dmenu
+make
+make install
+cd ..
+cd slstatus
+make
+make install
+
+cd .xinitrc ~/
